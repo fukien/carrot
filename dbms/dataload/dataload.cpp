29c0d2a7ca12b46d16c2aa4c91fccb2a7ce2a9a9
Huang Wentao <huangwentao0831@163.com>
2018-01-06 17:11:25 +0800

    fix a bug

--- dbms/dataload/dataload.cpp
+++ dbms/dataload/dataload.cpp
@@ -27,39 +27,6 @@
 
 void fillMetaSegmentDataload(TableMeta* meta,MetaDataSegment *ms)
 {
-    /* //SUPPLIER
-    meta->fieldNum= 7;
-    strcpy(meta->tname,"SUPPLIER");
-    meta->tid=3;
-    FieldPart* parts = meta->head;
-    if(parts->fieldList!=0)
-    {
-        delete parts->fieldList;
-    }
-    parts->partNum=7;
-    parts->tid=3;
-     parts->fieldList = new Field[7];
-     int leng[]={4,28,40,4,16,4,104};
-     int stype[]={DataTypeFlag::INTEGER,DataTypeFlag::CHAR,DataTypeFlag::VARCHAR,
-     DataTypeFlag::INTEGER,DataTypeFlag::CHAR,DataTypeFlag::FLOAT,DataTypeFlag::VARCHAR
-     };
-     char name[7][MAX_TABLE_NAME_LEN]={"S_SUPPKEY","S_NAME","S_ADDRESS","S_NATIONKEY","S_PHONE","S_ACCTBAL","S_COMMENT"};
-     int offset = 0;
-     for(int i=0;i<7;i++)
-     {
-         parts->fieldList[i].fid=i;
-         parts->fieldList[i].dataType=stype[i];
-         parts->fieldList[i].flag=0;
-        strcpy(parts->fieldList[i].fname,name[i]);
-         //parts->fieldList[i].fname=;
-         parts->fieldList[i].len=leng[i];
-         parts->fieldList[i].offsetInTuple=offset;
-         offset +=parts->fieldList[i].len;
-     }
-     parts->next=0;
-     if(ms!=0)ms->createOrUpdate();
-        */
-
    /*    //NATION
     meta->fieldNum=4 ;
     strcpy(meta->tname,"NATION");
@@ -159,6 +126,41 @@ void fillMetaSegmentDataload(TableMeta* meta,MetaDataSegment *ms)
      parts->next=0;
      if(ms!=0)ms->createOrUpdate();
 */
+
+
+ //SUPPLIER
+    meta->fieldNum= 7;
+    strcpy(meta->tname,"SUPPLIER");
+    meta->tid=3;
+    FieldPart* parts = meta->head;
+    if(parts->fieldList!=0)
+    {
+        delete parts->fieldList;
+    }
+    parts->partNum=7;
+    parts->tid=3;
+     parts->fieldList = new Field[7];
+     int leng[]={4,28,40,4,16,4,104};
+     int stype[]={DataTypeFlag::INTEGER,DataTypeFlag::CHAR,DataTypeFlag::VARCHAR,
+     DataTypeFlag::INTEGER,DataTypeFlag::CHAR,DataTypeFlag::FLOAT,DataTypeFlag::VARCHAR
+     };
+     char name[7][MAX_TABLE_NAME_LEN]={"S_SUPPKEY","S_NAME","S_ADDRESS","S_NATIONKEY","S_PHONE","S_ACCTBAL","S_COMMENT"};
+     int offset = 0;
+     for(int i=0;i<7;i++)
+     {
+         parts->fieldList[i].fid=i;
+         parts->fieldList[i].dataType=stype[i];
+         parts->fieldList[i].flag=0;
+        strcpy(parts->fieldList[i].fname,name[i]);
+         //parts->fieldList[i].fname=;
+         parts->fieldList[i].len=leng[i];
+         parts->fieldList[i].offsetInTuple=offset;
+         offset +=parts->fieldList[i].len;
+     }
+     parts->next=0;
+     if(ms!=0)ms->createOrUpdate();
+
+
 
    /* // PARTSUPP
    meta->fieldNum=5 ;
@@ -267,7 +269,7 @@ void fillMetaSegmentDataload(TableMeta* meta,MetaDataSegment *ms)
      if(ms!=0)ms->createOrUpdate();
         */
 
-             // LINEITEM
+  /*           // LINEITEM
    meta->fieldNum=16;
     strcpy(meta->tname,"LINEITEM");
     meta->tid=7;
@@ -305,7 +307,7 @@ void fillMetaSegmentDataload(TableMeta* meta,MetaDataSegment *ms)
      }
      parts->next=0;
      if(ms!=0)ms->createOrUpdate();
-
+        */
 }
 
 void packageLoad(Tuple* tuple,char** mp,TableMeta* meta) // transfer strings to columns
@@ -322,22 +324,23 @@ void packageLoad(Tuple* tuple,char** mp,TableMeta* meta) // transfer strings to 
 
 int writedbDataload()
 {
-        //char s[] = "data/supplier.tb";
-        //char data[] = "benchmark_data/benchmark_data/supplier.tbl";
+
        //char s[] = "data/nation.tb";
        //char data[] = "benchmark_data/benchmark_data/nation.tbl";
         //char s[] = "data/region.tb";
         //char data[] = "benchmark_data/benchmark_data/region.tbl";
         //char s[] = "data/part.tb";
        // char data[] = "benchmark_data/benchmark_data/part.tbl";
+        char s[] = "data/supplier.tb";
+        char data[] = "benchmark_data/benchmark_data/supplier.tbl";
        //char s [] = "data/partsupp.tb";
        //char data[] = "benchmark_data/benchmark_data/partsupp.tbl";
         //char s [] = "data/customer.tb";
         //char data [] = "benchmark_data/benchmark_data/customer.tbl";
         //char s [] = "data/orders.tb";
         //char data [] = "benchmark_data/benchmark_data/orders.tbl";
-        char s [] = "data/lineitem.tb";
-        char data [] = "benchmark_data/benchmark_data/lineitem.tbl";
+        //char s [] = "data/lineitem.tb";
+        //char data [] = "benchmark_data/benchmark_data/lineitem.tbl";
 
         Table* table = new Table();
         table->open(s,false);
@@ -392,7 +395,7 @@ int writedbDataload()
          //string filename="";
 }
 
-int mainDataLoad()
+int mainDataload()
 {
     writedbDataload();
 }
